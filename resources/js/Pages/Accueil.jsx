4e64bae71195d39a6e4884d96f029675f13e1b84
Antoine <ardyn142@gmail.com>
2025-07-28 10:23:04 -0400

    first commit

---
+++ resources/js/Pages/Accueil.jsx
@@ -0,0 +1,8 @@
+export default function Accueil() {
+    return (
+        <div>
+            <h1 className="text-9xl">Bienvenue sur la page d'accueil</h1>
+            <p>Ceci est le contenu de la page d'accueil.</p>
+        </div>
+    );
+}
